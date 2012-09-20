c72ddc481c9f2be3c02ca69ac4a43ee0950a334e
Tyler Rigsby <tjrigs@gmail.com>
2012-09-20 13:48:28 -0700

    v1_2 release
    Changes to requests for stability, added settings pane.

--- firenze/main.py
+++ firenze/main.py
@@ -16,6 +16,7 @@
 #
 from google.appengine.api import users
 from google.appengine.api import memcache
+from google.appengine.api import channel
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
